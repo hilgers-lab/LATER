32c8ddf0f5ac979c41ea5d8a3ac529e99cfd0d48
cag1343 <62571036+cag1343@users.noreply.github.com>
2022-12-16 18:07:51 +0100

    Update estimateTranscriptionalBias.R

--- R/estimateTranscriptionalBias.R
+++ R/estimateTranscriptionalBias.R
@@ -20,6 +20,7 @@ estimateTranscriptionalBias  <-
       promoterDominanceTable %>% filter(gene_id %in% atss.apa.genes) %>% dplyr::select(gene_id, tes_id, pairs_sum, promoter_id)
     perGeneList <- split(headData, f = headData$gene_id)
     couplingsmatrix <- lapply(perGeneList, function(x) {
+     # Produce combination matrix of every TES and Promoter of the gene 
       x1 <-
         x %>% maditr::dcast(tes_id ~ promoter_id, value.var = "pairs_sum") %>% as.data.frame(.)
       rownames(x1) <- x1$tes_id
@@ -31,10 +32,11 @@ estimateTranscriptionalBias  <-
         x[is.na(x)] <- 0
         x
       })
+    # Add pseudocount  
     couplingsmatrix <- lapply(couplingsmatrix, function(x) {
       x + 0.7
     })
-
+    # For every per gene matrix perform chisq test and monte carlo simulation to obtain better p-value estimates. 
     if(method=="chisq"){
       perGeneChisqTest <- lapply(couplingsmatrix, function(x) {
         x2 <- chisq.test(x, simulate.p.value = TRUE)
@@ -49,8 +51,9 @@ estimateTranscriptionalBias  <-
       return(affectedGenes)
 
     }else{
+      # For every per gene matrix perform fisher test and monte carlo simulation to obtain better p-value estimates.  
       perGeneFisher <- lapply(couplingsmatrix, function(x) {
-        res.fish <- fisher.test(x, simulate.p.value = TRUE)
+        res.fish <- fisher.test(x, simulate.p.value = TRUE) 
         res.fish <- res.fish$p.value
         return(res.fish)
       })
